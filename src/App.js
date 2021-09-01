2f33667cb300bbea33c7f76f99bfb7d9fcfed0fd
Chris <chris.ware.dev@gmail.com>
2021-09-01 07:24:11 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  return;
+  <>
+    <h2>color generator project</h2>
+    <h1>Hello</h1>
+  </>;
+}
+
+export default App;
